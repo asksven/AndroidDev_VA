01abd27574b7501ffe445014159d8c6f3523aeac
asksven <sven.knispel@gmail.com>
2016-06-19 23:28:23 +0200

    Tested and fixed the installation Test passed 2016-06-19 on linux mint 17.3

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-AndroidDev_VA
-=============
+# AndroidDev_VA / LinuxDev
 
-A generic Dev virtual appliance, for Android, Node and Azure
+A generic Dev virtual appliance, for Android, Node, cordova/ionic and Docker
+
+## Know Issues
+2016-06-19: currently the ppa for android-studio does not add the icon to the desktop. To start android-studio: /opt/android-studio/bin/studio.sh
+
+
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # install open-jdk
-sudo apt-get install openjdk-7-jdk git chromium-browser curl
+sudo apt-get install --assume-yes openjdk-7-jdk git chromium-browser curl
 
 
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - && sudo apt-get install --assume-yes nodejs && sudo apt-get install --assume-yes npm build-essential
 
-sudo apt-get install nodejs build-essential
 
 sudo npm install -g cordova
 
@@ -14,19 +13,19 @@ sudo npm install -g ionic
 
 
 # install atom
-sudo add-apt-repository ppa:webupd8team/atom && sudo apt-get update && sudo apt-get install atom
+sudo add-apt-repository -y ppa:webupd8team/atom && sudo apt-get update && sudo apt-get --assume-yes install atom
 
 # Docker: follow instructions here: https://docs.docker.com/linux/step_one/
 
 curl -fsSL https://get.docker.com/ | sh
 
 #install android studio
-sudo apt-get install ia32-libs && sudo apt-add-repository ppa:paolorotolo/android-studio && sudo apt-get update && sudo apt-get install android-studio
+sudo apt-get install --assume-yes ia32-libs && sudo apt-add-repository -y ppa:paolorotolo/android-studio && sudo apt-get update && sudo apt-get --assume-yes install android-studio
 
 
 # install adb
-# sudo apt-get install android-tools-adb
+# sudo apt-get install -y android-tools-adb
 
 
 # Update mint
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade --assume-yes
